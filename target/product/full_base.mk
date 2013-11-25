3fe6a3d12948ba7e5261f33769e458eba3800b88
kowcop-bg <kowcop+bg@gmail.com>
2013-11-25 18:17:34 +1100

    debloat

--- target/product/full_base.mk
+++ target/product/full_base.mk
@@ -21,18 +21,9 @@
 
 PRODUCT_PACKAGES := \
     libfwdlockengine \
-    VideoEditor \
     WAPPushManager
 
 PRODUCT_PACKAGES += \
-    Galaxy4 \
-    HoloSpiralWallpaper \
-    LiveWallpapers \
-    LiveWallpapersPicker \
-    MagicSmokeWallpapers \
-    NoiseField \
-    PhaseBeam \
-    VisualizationWallpapers \
     PhotoTable
 
 # Additional settings used in all AOSP builds
